91c411880068ddb77db32c1f1c98222dcd88d6f6
Jaywanth J <54073644+jaywanthgowda@users.noreply.github.com>
2023-04-07 21:31:46 +0530

    add: problem.py

---
+++ problem.py
@@ -0,0 +1,4 @@
+# Write a function that takes a list of n-1 integers (in the range from 1 to n) as input and returns the missing number.
+# The array need not be sorted
+
+# Input : [1,4,2] -> Output: 3
