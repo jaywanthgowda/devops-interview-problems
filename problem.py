0ed6fc9d25bea261af02bfd8a7ee32d266ec9547
Jaywanth J <54073644+jaywanthgowda@users.noreply.github.com>
2023-04-07 21:28:42 +0530

    add: problem.py

---
+++ problem.py
@@ -0,0 +1,4 @@
+# Given an array of strings , group the anagrams together. You can return the answer in any order.
+
+# Input : ["eat","tea","tan","ate","nat","bat"]
+# Output : [["bat"],["nat","tan"],["ate","eat","tea"]]
