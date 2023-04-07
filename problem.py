8c98ebab4c865f495d4f353078e1219289dfb91e
Jaywanth J <54073644+jaywanthgowda@users.noreply.github.com>
2023-04-07 22:05:32 +0530

    add: problem.py

---
+++ problem.py
@@ -0,0 +1,5 @@
+# Given two sorted arrays of integers
+# Write a function that returns a new sorted array that contains all the elements from both arrays
+
+# Input: A = [1,2,4,7,8] , B = [2,5,6,9]
+# Output: Combined = [1,2,2,4,5,6,7,8,9]
